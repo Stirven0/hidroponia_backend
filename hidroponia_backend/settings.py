2078e3a1f14cb7fa324c0e4e12e99c4dd62b551b
Stirven0 <juliocesaramaya91@hotmail.com>
2025-11-10 21:07:43 -0500

    correciones

--- hidroponia_backend/settings.py
+++ hidroponia_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'cultivos',
     'sensores',
     'actuadores',
@@ -73,7 +74,11 @@ TEMPLATES = [
     },
 ]
 
-
+# REST_FRAMEWORK = {
+#     'DEFAULT_RENDERER_CLASSES': [
+#         'rest_framework.renderers.JSONRenderer',
+#     ],
+# }
 
 WSGI_APPLICATION = 'hidroponia_backend.wsgi.application'
 
@@ -84,11 +89,11 @@ WSGI_APPLICATION = 'hidroponia_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME', default='proyecto_hidroponico'),
-        'USER': config('DB_USER', default='root'),
-        'PASSWORD': config('DB_PASSWORD', default='1234'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3307'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
         'OPTIONS': {
             'charset': 'utf8mb4',
         },
